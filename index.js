dbb3bc9a778db3162387e6cab575c81f11d78d55
Amen Moja Ra <mojaray2k@gmail.com>
2021-07-11 19:58:48 +0000

    Implemented reverseArrayInPlace algorithm

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 function reverseArrayInPlace(arr) {
   // reverse arr
   // return reversed arr
+
+  for(var i = 0; i < arr.length / 2; i++){
+    var tempVar = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = tempVar;
+  }
+
+  return arr;
 }
+
+reverseArrayInPlace([1,2,3,4,5,6,7])
